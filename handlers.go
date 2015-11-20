48a2d928c714ad24632e01637f88e072f4018034
Martin Garton <martin.garton@ft.com>
2015-11-20 16:08:48 +0000

    Run checks every minute in the background.
    Whenever things are busy/broken everyone (understandably) jumps onto the
    aggregate healthcheck to see whats wrong.  For each request, we kick of
    n*more requests to the back end apps.  The end result contributes to
    massive network usage, and we've seen tens of thousands of tcp
    connections in the agg hc docker container.
    
    This change means we update a stored view of service health every
    minute, and when requesting we always return the same cached result
    immediately.

--- handlers.go
+++ handlers.go
@@ -4,18 +4,24 @@ import (
 	"encoding/json"
 	"fmt"
 	fthealth "github.com/Financial-Times/go-fthealth/v1a"
+	"log"
 	"net/http"
+	"time"
 )
 
 type hchandlers struct {
-	registry    ServiceRegistry
-	checker     ServiceHealthChecker
-	name        string
-	description string
+	registry     ServiceRegistry
+	checker      ServiceHealthChecker
+	name         string
+	description  string
+	latestResult <-chan fthealth.HealthResult
 }
 
 func NewHCHandlers(registry ServiceRegistry, checker ServiceHealthChecker) *hchandlers {
-	return &hchandlers{registry, checker, "Coco Aggregate Healthcheck", "Checks the health of all deployed services"}
+	lr := make(chan fthealth.HealthResult)
+	hch := &hchandlers{registry, checker, "Coco Aggregate Healthcheck", "Checks the health of all deployed services", lr}
+	go hch.loop(lr)
+	return hch
 }
 
 func (hch *hchandlers) handle(w http.ResponseWriter, r *http.Request) {
@@ -27,16 +33,36 @@ func (hch *hchandlers) handle(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (hch *hchandlers) result() fthealth.HealthResult {
-	checks := []fthealth.Check{}
-	for _, service := range hch.registry.Services() {
-		checks = append(checks, NewCocoServiceHealthCheck(service, hch.checker))
+func (hch *hchandlers) loop(lr chan<- fthealth.HealthResult) {
+
+	newResult := make(chan fthealth.HealthResult)
+
+	go func() {
+		for {
+			checks := []fthealth.Check{}
+			for _, service := range hch.registry.Services() {
+				checks = append(checks, NewCocoServiceHealthCheck(service, hch.checker))
+			}
+			start := time.Now()
+			health := fthealth.RunCheck(hch.name, hch.description, true, checks...)
+			log.Printf("got new health results in %v\n", time.Now().Sub(start))
+			newResult <- health
+			time.Sleep(60 * time.Second)
+		}
+	}()
+
+	var latest fthealth.HealthResult
+	for {
+		select {
+		case latest = <-newResult:
+		case lr <- latest:
+		}
+
 	}
-	return fthealth.RunCheck(hch.name, hch.description, true, checks...)
 }
 
 func (hch *hchandlers) jsonHandler(w http.ResponseWriter, r *http.Request) {
-	health := hch.result()
+	health := <-hch.latestResult
 
 	w.Header().Set("Content-Type", "application/json")
 	enc := json.NewEncoder(w)
@@ -63,7 +89,7 @@ func (hch *hchandlers) htmlHandler(w http.ResponseWriter, r *http.Request) {
 	serviceHtmlTemplate := "<dd>- <a href=\"%s\">%s</a>  : %s</dd>"
 	servicesHtml := ""
 
-	checkResult := hch.result()
+	checkResult := <-hch.latestResult
 	for _, check := range checkResult.Checks {
 		serviceHealthUrl := fmt.Sprintf("/health/%s/__health", check.Name)
 		if !check.Ok {
